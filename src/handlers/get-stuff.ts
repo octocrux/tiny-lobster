c007506df2d903042ce8ec9e9ccab6126038c84f
octocrux <cate.petrusevich@gmail.com>
2021-10-05 18:48:56 +0300

    move static/ one level up

--- src/handlers/get-stuff.ts
+++ src/handlers/get-stuff.ts
@@ -6,7 +6,7 @@ import { readCornerLines } from '../util/read-corner-lines'
 
 export const getStuff: RequestHandler = (req, res) => {
   const url = new URLSearchParams(req.query as Record<string, string>)
-  const fileName = path.resolve(__dirname, '../static/stuff.txt')
+  const fileName = path.resolve(__dirname, '../../static/stuff.txt')
 
   readCornerLines(fileName).then(({ firstLine, lastLine }) => {
     res.status(200).json({
