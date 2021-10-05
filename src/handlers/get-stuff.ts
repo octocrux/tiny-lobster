e0c13836a23f4cb3cefa8ef064bbc5715bc4b2be
octocrux <cate.petrusevich@gmail.com>
2021-10-05 22:15:27 +0300

    return search parameters as is

--- src/handlers/get-stuff.ts
+++ src/handlers/get-stuff.ts
@@ -1,13 +1,12 @@
 import { RequestHandler } from 'express'
 import path from 'path'
-import { URLSearchParams } from 'url'
 
 import { logger } from '../config/logger'
 import { readCornerLines } from '../util/read-corner-lines'
 
 export const getStuff: RequestHandler = (req, res) => {
   logger.info('handler: getStuff')
-  const url = new URLSearchParams(req.query as Record<string, string>)
+
   const fileName = path.resolve(__dirname, '../../static/stuff.txt')
 
   readCornerLines(fileName).then(({ firstLine, lastLine }) => {
@@ -16,7 +15,7 @@ export const getStuff: RequestHandler = (req, res) => {
     res.status(200).json({
       firstLine,
       lastLine,
-      params: url.toString()
+      searchParams: req.query
     })
   })
 }
